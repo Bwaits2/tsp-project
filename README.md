b0d1ebaa504ee6214b77588992e43549de3ff07e
AlexKElliott <aelliott41@gatech.edu>
2020-04-13 16:44:15 -0400

    Added timer, lookup table, removed unnecessary deep copies

--- README.md
+++ README.md
@@ -6,9 +6,8 @@ http://www.theprojectspot.com/tutorial-post/simulated-annealing-algorithm-for-be
 
 todo:
 1. add timer like in the pdf
-2. precompute distance values?
-3. reduce floats to ints or round them during computations
-4. clean up code
+2. clean up code
+3. get rid of the second array, n, in sa()
 
 potential ideas:
 1. writing a script that runs tsp-3510 a bunch of times to get an idea for the best t and cr
--- tsp-3510.py
+++ tsp-3510.py
@@ -3,9 +3,13 @@ import random
 import sys
 
 from statistics import stdev, mean
+from time import perf_counter
+import signal
 
 # globals
 nodes = []
+distances = []
+time_elapsed = 0.0;
 
 # build list from txt file
 def build():
@@ -13,10 +17,27 @@ def build():
 
     for l in f:
         s = l.split(' ')
-        nodes.append([float(s[1]), float(s[2]), s[0]])
+        nodes.append([float(s[1]), float(s[2]), int(s[0])])
 
     f.close()
 
+    # distances = [[None for x in range(len(nodes))] for x in range(len(nodes))]
+    for x in range(len(nodes)):
+        distances.append([])
+        for y in range(len(nodes)):
+            distances[x].append(None)
+
+    #populate distances
+    for i in range(len(nodes)):
+        for j in range(len(nodes)):
+            if i == j:
+                distances[i][j] = 0
+
+            elif distances[j][i] is not None:
+                distances[i][j] = distances[j][i]
+
+            else:
+                distances[i][j] = int(math.sqrt((nodes[i][0] - nodes[j][0])**2 + (nodes[i][1] - nodes[j][1])**2))
 
 # distance funciton to compute distance of the entire tour
 def distance(t):
@@ -24,8 +45,8 @@ def distance(t):
 
     i = 1
     while i < len(t):
-    	result += math.sqrt((t[i][0] - t[i-1][0])**2 + (t[i][1] - t[i-1][1])**2)
-    	i+=1
+        result += distances[t[i][2] - 1][t[i-1][2] - 1]
+        i += 1
 
     return result
 
@@ -40,19 +61,19 @@ def result(best):
 
     f.close()
 
-
+ 
 def compute():
-	results = []
+    results = []
+    for i in range(10):
+        sa_result = sa()
+        if sa_result is not None:
+            results.append(distance(sa_result))
+        else:
+            break #None means time expired
 
-	for i in range(10):
-		results.append(distance(sa()))
-
-	print(results)
-
-	print("stdev: " + str(stdev(results)))
-	print("mean: " + str(mean(results)))
-
-
+    print(results)
+    print("stdev: " + str(stdev(results)))
+    print("mean: " + str(mean(results)))
 
 # anneal function
 def sa():
@@ -60,37 +81,64 @@ def sa():
     cr = .0001
 
     current = nodes.copy()
-    best = current.copy()
+    best = current
 
     while t > 1:
-    	new = current.copy()
+        new = current.copy()
 
-    	p1 = random.randint(0, len(new) - 1)
-    	p2 = random.randint(0, len(new) - 1)
+        # p1 = random.randint(0, len(new) - 1)
+        # p2 = random.randint(0, len(new) - 1)
 
-    	new[p1], new[p2] = new[p2], new[p1]
+        #This is just a test, I dont want to handle edge case of edge swapping so fix this
+        p1 = random.randint(1, len(new) - 2)
+        p2 = p1
+        while (p2 == p1):
+            p2 = random.randint(1, len(new) - 2)
 
-    	c = distance(current)
-    	n = distance(new)
+        new[p1], new[p2] = new[p2], new[p1]
 
-    	try:
-    		if math.exp((c-n)/t) > random.random():
-    			current = new.copy()
-    	except OverflowError:
-    		pass
+        c = distance(current)
 
-    	if n < c:
-    		best = new.copy()
+        curr_p1prev = current[p1 - 1][2] - 1
+        curr_p1 = current[p1][2] - 1
+        curr_p1next = current[p1 + 1][2] - 1
 
-    	t *= 1 - cr
+        curr_p2prev = current[p2 - 1][2] - 1
+        curr_p2 = current[p2][2] - 1
+        curr_p2next = current[p2 + 1][2] - 1
+
+        #sequential nodes p1 < p2 
+        if (p1 - p2 == -1):
+            c_removed = (distances[curr_p1prev][curr_p1] + distances[curr_p2][curr_p2next]) 
+            n_added = (distances[curr_p1prev][curr_p2] + distances[curr_p1][curr_p2next])
+
+        #sequential nodes p1 > p2 
+        elif (p1 - p2 == 1):
+            c_removed = (distances[curr_p2prev][curr_p2] + distances[curr_p1][curr_p1next]) 
+            n_added = (distances[curr_p2prev][curr_p1] + distances[curr_p2][curr_p1next])
+
+        #non-sequential nodes
+        else:
+            c_removed = distances[curr_p1prev][curr_p1] + distances[curr_p1][curr_p1next] + distances[curr_p2prev][curr_p2] + distances[curr_p2][curr_p2next] 
+            n_added = distances[curr_p1prev][curr_p2] + distances[curr_p2][curr_p1next] + distances[curr_p2prev][curr_p1] + distances[curr_p1][curr_p2next]
+        
+        n = c - c_removed + n_added
+
+        try:
+            if math.exp((c-n)/t) > random.random():
+                current = new
+        except OverflowError:
+            pass
+
+        if n < c:
+            best = new
+
+        t *= 1 - cr
 
     return best
 
-
-
+curr_time = perf_counter()
 build()
-#print("distance before: " + str(distance(nodes)))
 compute()
-#b = sa()
-#print("distance after: " + str(distance(b)))
-#result(b)
+end_time = perf_counter()
+print("time elapsed: ", end_time - curr_time)
